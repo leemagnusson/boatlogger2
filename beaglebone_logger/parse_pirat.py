9ce7a380cf1f25ecb4ef11cfc90605c270ef9c61
leemagnusson@gmail.com <leemagnusson@gmail.com@f89a8b19-fc4c-1111-860b-ebfafb781f11>
2013-05-12 11:21:38 +0000

    parsing wind and sow
    git-svn-id: http://boatlogger.googlecode.com/svn/trunk@135 f89a8b19-fc4c-1111-860b-ebfafb781f11

--- beaglebone_logger/parse_pirat.py
+++ beaglebone_logger/parse_pirat.py
@@ -12,30 +12,32 @@ fileout = "data.csv"
 parser = Parser('lessgps/data/nmea.yaml')
 
 sentence_filter = ['VHW','MWV']
-out_format = [[['WindAngle','Value'],['WindAngle','Ref'],['WindSpeed','Value'],['WindSpeed','Ref']],
- [['DegreesTrue','Value'],['DegreesTrue','Ref'],['SOW','Value'],['SOW','Ref']]]
+out_format = [[['DegreesTrue','Value'],['DegreesTrue','Ref'],['SOW','Value'],['SOW','Ref']],[['WindAngle','Value'],['WindAngle','Ref'],['WindSpeed','Value'],['WindSpeed','Unit']]]
 
 def format_out(d,form):
     l = []
     for f in form:
-        l += str(reduce(dict.get, f, d))
+        l.append(str(reduce(dict.get, f, d)))
     print l
-    return l.join(',')
+    return ','.join(l) + '\n'
 
 #with open(fileout,'w') as fout:
-with [open(fileout + '.csv','w') for fileout in sentence_filter] as fout:
-    for fname in fnames:
-        print fname
-        with open(fname,'r') as f:
-            for sentence in f:
-                sentence = sentence.strip()
-                #print sentence[1:6]
-                if sentence:
-                    if sentence[3:6] in sentence_filter:
-                        ind = sentence_filter.index(sentence[3:6])
-                        try:
-                            p = parser.parse(sentence)
-                            #fout.write("{0}\n".format(p['Temp']['Value']))
-                            fout[ind].write(format_out(p,out_format[ind]))
-                        except pyparsing.ParseException as e:
-                            print e
+#with [open(fileout + '.csv','w') for fileout in sentence_filter] as fout:
+fout = [open(fileout + '.csv','w') for fileout in sentence_filter]
+
+for fname in fnames:
+    print fname
+    with open(fname,'r') as f:
+        for sentence in f:
+            sentence = sentence.strip()
+            #print sentence[1:6]
+            if sentence:
+                if sentence[3:6] in sentence_filter:
+                    ind = sentence_filter.index(sentence[3:6])
+                    try:
+                        p = parser.parse(sentence)
+                        #fout.write("{0}\n".format(p['Temp']['Value']))
+                        fout[ind].write(format_out(p,out_format[ind]))
+                    except pyparsing.ParseException as e:
+                        print e
+                        print sentence
